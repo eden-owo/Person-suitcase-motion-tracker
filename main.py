e3d0bbd69c44b3b8e119c236298e747e42c4626a
eden-owo <edenwu0625@gmail.com>
2025-06-13 15:22:41 +0800

    add visualize

--- main.py
+++ main.py
@@ -26,34 +26,7 @@ from ultralytics.utils.checks import check_yaml
 
 from yolo.yolo_seg import YOLOv8Seg
 from utils.transform import RP
-
-def draw_box_and_mask(img, box, mask, label, color):
-    if not isinstance(img, np.ndarray):
-        raise TypeError(f"img 必須是 numpy.ndarray，目前是 {type(img)}")
-    """
-    繪製 bbox, label 和對應的 segmentation mask。
-
-    Parameters:
-        img: 原始影像（np.ndarray）
-        box: bbox 座標 (x1, y1, x2, y2)
-        mask: mask 二值影像（np.ndarray）
-        label: 要顯示的文字
-        color: RGB 顏色 (tuple)
-
-    Returns:
-        img: 處理後影像（np.ndarray）
-    """
-    x1, y1, x2, y2 = map(int, box)
-    cv2.rectangle(img, (x1, y1), (x2, y2), color, 1)
-    cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
-    mask = mask.cpu().numpy().astype(np.uint8) * 255
-    mask_color = np.zeros_like(img, dtype=np.uint8)
-    mask_color[:, :] = color
-    masked = cv2.bitwise_and(mask_color, mask_color, mask=mask)
-    img = cv2.addWeighted(img, 1.0, masked, 0.5, 0)
-
-    return img
+from utils.visualize import draw_box_and_mask
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
