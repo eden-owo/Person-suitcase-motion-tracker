b8cf67aadc4a0f9246ae0b7a0c3cee26cae1bae9
eden-owo <edenwu0625@gmail.com>
2025-07-11 11:04:12 +0800

    add main_test.py

--- main_test.py
+++ main_test.py
@@ -1,13 +1,14 @@
 from ultralytics import YOLO
 
 # Load your YOLO model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolo11m-seg.pt')
+
 
 # Export to TensorRT format
 model.export(format='engine')
 
 # Load the TensorRT model
-trt_model = YOLO('yolov8n.engine')
+trt_model = YOLO('yolo11m-seg.engine')
 
 # Run inference using the TensorRT model
 results = trt_model('test/suitcase3.mp4')
